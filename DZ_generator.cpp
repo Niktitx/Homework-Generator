d513cb7da4374440b71b80dd4cd32f9368e796eb
Niktitx <niktitx@gmail.com>
2025-11-28 16:12:05 +0300

    More readable code

--- DZ_generator.cpp
+++ DZ_generator.cpp
@@ -1,104 +1,141 @@
 #include <chrono>
-#include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <random>
 #include <string>
-
+#include <vector>
 using namespace std;
 
-int CurrentWeek;
-mt19937 gen(static_cast<unsigned int>(
-    std::chrono::high_resolution_clock::now().time_since_epoch().count()));
-uniform_int_distribution<int> dist;
+enum Types { ORAL, EXERCISE, NUMBERS, NO_TASK };
 
-enum Types { oral, exerсise, numbers, no_task };
-struct Object {
-  string Name;
+struct Lesson {
+  string name;
   Types type;
   int times_per_week = 0;
-
-  Object(string _name, Types _type) : Name(_name), type(_type) {};
 };
 
-Object lessons[] = {
-    {"Физика", oral},      {"История", oral},     {"Математика", numbers},
-    {"Физра", no_task},    {"Биология", oral},    {"Бел яз", exerсise},
-    {"Англ яз", exerсise}, {"Бел лит", no_task},  {"Химия", oral},
-    {"Русс лит", no_task}, {"Русс яз", exerсise}, {"ДМП", no_task},
-    {"Информатика", oral}, {"Астрономия", oral},  {"Общество", oral},
-    {"География", oral},   {"", no_task}
+class Generator {
+  mt19937 gen;
+  int CurrentWeek;
+  vector<vector<int>> schedule;
+  vector<Lesson> lessons = {
+      {"", NO_TASK},         {"Англ яз", EXERCISE},   {"Астрономия", ORAL},
+      {"Бел лит", NO_TASK},  {"Бел яз", EXERCISE},    {"Биология", ORAL},
+      {"ДМП", NO_TASK},      {"География", ORAL},     {"Информатика", ORAL},
+      {"История", ORAL},     {"Математика", NUMBERS}, {"Общество", ORAL},
+      {"Русс лит", NO_TASK}, {"Русс яз", EXERCISE},   {"Физика", ORAL},
+      {"Физра", NO_TASK},    {"Химия", ORAL}};
+  ;
 
-};
-
-const int schedule[5][7] = {{1, 2, 3, 0, 4, 5, 16},
-                            {6, 5, 7, 2, 0, 8, 16},
-                            {9, 2, 0, 10, 4, 2, 3},
-                            {3, 11, 2, 6, 13, 8, 12},
-                            {14, 1, 2, 9, 0, 15, 16}};
-
-void calculateTimesPerWeek() {
-  for (int i = 0; i < 5; i++)
-    for (int j = 0; j < 7; j++)
-      lessons[schedule[i][j]].times_per_week++;
-}
-
-string CreateNumbers(int n, int min, int max) {
-  string s = "№ ";
-  dist = uniform_int_distribution<int>(min, max);
-  for (int i = 0; i < n; i++)
-    s += to_string(dist(gen)) + ", ";
-  s.pop_back();
-  s.pop_back();
-  return s;
-}
-
-void GenerateTask(int id, int n) {
-  if (id == 16)
-    return;
-  string task;
-
-  dist = uniform_int_distribution<int>(-1, 1);
-  switch (lessons[id].type) {
-  case oral:
-    task = "$" + to_string(CurrentWeek * min(lessons[id].times_per_week, 2) +
-                           dist(gen));
-    if (lessons[id].Name == "Химия")
-      task += ", " + CreateNumbers(3, 1, 10);
-    else if (lessons[id].Name == "Физика") {
-      task += ", " + CreateNumbers(3, 1000, 2000);
-    }
-    break;
-  case exerсise:
-    dist = uniform_int_distribution<int>(-10, 10);
-    if (lessons[id].Name == "Бел яз")
-      task = "пр " + to_string(CurrentWeek * 20 + dist(gen));
-    else if (lessons[id].Name == "Англ яз")
-      task = "SB: p." + to_string(CurrentWeek * 15 + dist(gen)) + " ex." +
-             to_string(abs(dist(gen) / 2));
-    else
-      task = "упр " + to_string(CurrentWeek * 20 + dist(gen));
-    break;
-  case numbers:
-    task = CreateNumbers(3, 1000, 3500);
-    break;
-  case no_task:
-    break;
+  void calculateTimesPerWeek() {
+    for (auto &day : schedule)
+      for (int id : day)
+        if (id != -1)
+          lessons[id].times_per_week++;
   }
 
-  cout << n + 1 << ". " << lessons[id].Name << "\t" << task << endl;
-}
+  string randNumbers(int n, int min, int max) {
+    uniform_int_distribution<int> dist(min, max);
+    string s = "№ ";
+    for (int i = 0; i < n; i++) {
+      s += to_string(dist(gen));
+      if (i + 1 != n)
+        s += ", ";
+    }
+    return s;
+  }
+
+  string generateTask(int id) {
+    if (id == -1)
+      return "";
+
+    const Lesson &L = lessons[id];
+    uniform_int_distribution<int> d(-1, 1);
+
+    switch (L.type) {
+    case ORAL: {
+      string task =
+          "$" + to_string(CurrentWeek * min(L.times_per_week, 2) + d(gen));
+      if (L.name == "Химия")
+        task += ", " + randNumbers(3, 1, 10);
+      if (L.name == "Физика")
+        task += ", " + randNumbers(3, 1000, 2000);
+      return task;
+    }
+    case EXERCISE: {
+      uniform_int_distribution<int> dev(-10, 10);
+      int base = CurrentWeek * 20 + dev(gen);
+
+      if (L.name == "Бел яз")
+        return "пр " + to_string(base);
+      if (L.name == "Англ яз")
+        return "SB: p." + to_string(CurrentWeek * 15 + dev(gen)) + " ex." +
+               to_string(abs(dev(gen) / 2) + 1);
+
+      return "упр " + to_string(base);
+    }
+    case NUMBERS:
+      return randNumbers(3, 1000, 3500);
+
+    case NO_TASK:
+    default:
+      return "";
+    }
+  }
+
+public:
+  Generator()
+      : gen(chrono::high_resolution_clock::now().time_since_epoch().count()) {}
+
+  bool loadSchedule(const string &filename) {
+    ifstream f(filename);
+    if (!f.is_open())
+      return false;
+
+    schedule.clear();
+    string line;
+    while (true) {
+      vector<int> row;
+      int x;
+      bool any = false;
+      for (int i = 0; i < 7; i++) {
+        if (!(f >> x))
+          return !schedule.empty();
+        if (x != -1)
+          any = true;
+        row.push_back(x);
+      }
+      schedule.push_back(row);
+    }
+  }
+
+  void runConsole() {
+    cout << "Введите номер параграфа: ";
+    cin >> CurrentWeek;
+
+    calculateTimesPerWeek();
+
+    for (int i = 0; i < schedule.size(); i++) {
+      cout << endl;
+      for (int j = 0; j < schedule[i].size(); j++) {
+        int id = schedule[i][j];
+        if (id == -1)
+          continue;
+
+        string task = generateTask(id);
+        cout << j + 1 << ". " << lessons[id].name << "\t" << task << "\n";
+      }
+    }
+  }
+};
 
 int main() {
-  cout << "Введите номер параграфа по предмету, который 1 раз в неделю: ";
-  cin >> CurrentWeek;
-  cout << endl;
-
-  calculateTimesPerWeek();
-
-  for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 7; j++)
-      GenerateTask(schedule[i][j], j);
-    cout << endl;
+  Generator g;
+  if (!g.loadSchedule("schedule.txt")) {
+    cout << "Ошибка загрузки schedule.txt\n";
+    return 1;
   }
+
+  g.runConsole();
   return 0;
 }
